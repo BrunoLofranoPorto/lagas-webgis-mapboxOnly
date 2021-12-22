67c8a0db5c794c716737d52b26cf29490f9ce9f8
BrunoLofranoPorto <89394173+BrunoLofranoPorto@users.noreply.github.com>
2021-12-22 10:13:59 -0300

    Add files via upload

---
+++ MarcadoresTD.js
@@ -0,0 +1,210 @@
+const marker = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-60.023, -3.104])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Transversalidade das políticas públicas na gestão de risco de inundações.</h1>"))
+    .addTo(map);
+
+const marker0 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-61.406, -3.579])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Transversalidade das políticas públicas na gestão de risco de inundações.</h1>"))
+    .addTo(map);
+
+const marker1 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.792, -15.648])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Distribuição espacial da hepatite a na região de Sobradinho/DF entre 2007 e 2015.</h1>"))
+    .addTo(map);
+
+const marker2 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.921, -15.806])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Padrões Espaciais do Suicídio no Distrito Federal.</h1>"))
+    .addTo(map);
+
+const marker3 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.784, -15.793])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Suicídio segundo grupos ocupacionais: uma descrição espaço-temporal no Distrito Federal (DF), Brasil.</h1>"))
+    .addTo(map);
+
+const marker4 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.622,-14.038])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Distribuição Espaço Temporal: focos de calor detectados por sensores AVHRR e Modis na microregião da Chapada dos Veadeiros.</h1>"))
+    .addTo(map);
+
+const marker5 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-51.834, 3.844])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Avaliação de políticas públicas de gestão do território a partir de um estudo sobre a fragmentação da paisagem nos municípios de Oiapoque e Calçoene, estado do Amapá.</h1>"))
+    .addTo(map);
+
+const marker6 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-50.948, 2.495])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Avaliação de políticas públicas de gestão do território a partir de um estudo sobre a fragmentação da paisagem nos municípios de Oiapoque e Calçoene, estado do Amapá.</h1>"))
+    .addTo(map);
+
+const marker7 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-45.601, -5.055])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Efetividade e a Dinâmica Territorial das Unidades de Conservação do Estado do Maranhão: novas perspectivas e outros olhares.</h1>"))
+    .addTo(map);
+
+const marker8 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-48.066, -15.806])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>A dinâmica da paisagem e as doenças urbanas : um ensaio teórico-conceitual sobre a paisagem como categoria de análise geográfica na integração entre saúde e ambiente</h1>"))
+    .addTo(map);
+
+const marker9 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-52.012, -2.881])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>A implantação de hidrelétricas na Amazônia brasileira, impactos socioambientais e à saúde com as transformações no território: o caso da UHE de Belo Monte.</h1>"))
+    .addTo(map);
+
+const marker10 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-36.953, -5.461])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Estudo dos padrões espaço-temporais de ocorrência da diarreia no semiárido brasileiro.</h1>"))
+    .addTo(map);
+
+const marker11 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-36.782, -7.234])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Estudo dos padrões espaço-temporais de ocorrência da diarreia no semiárido brasileiro.</h1>"))
+    .addTo(map);
+
+const marker12 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.980, -15.932])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Série de casos: caracterização da mobilidade e do acesso de crianças com síndrome congênita por Zika e Storch aos serviços de estimulação precoce no Distrito Federal.</h1>"))
+    .addTo(map);
+
+const marker13 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-50.511, -12.860])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Migrações Internacionais e Refúgio no Brasil entre 2000 e 2014: uma análise espaço-temporal.</h1>"))
+    .addTo(map);
+
+const marker14 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-55.264, -2.198])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Análise temporal do uso e cobertura da terra na bacia hidrográfica do Lago Grande do Curuai, Pará.</h1>"))
+    .addTo(map);
+
+const marker15 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.776, -15.901])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Relações entre a dengue e a Dinâmica urbana de São Sebastião, Distrito Federal.</h1>"))
+    .addTo(map);
+
+const marker16 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-62.658, -1.732])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Sistema de Informações Geográficas no Auxílio do Manejo do Pirarucu Arapaima gigas na Reserva Extrativista do Rio Unini, Barcelos - AM.</h1>"))
+    .addTo(map);
+
+const marker17 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.595, -15.703])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Avaliação do Sistema de Vigilância Epidemiológica da Dengue - SINAN no Distrito Federal, 2010-2015.</h1>"))
+    .addTo(map);
+
+const marker18 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-48.081, -15.621])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Dengue e desigualdades socioespaciais no Distrito Federal, Brasil.</h1>"))
+    .addTo(map);
+
+const marker19 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.860, -15.576])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Relações entre a dinâmica ambiental e a dengue no Distrito Federal, Brasil.</h1>"))
+    .addTo(map);
+
+const marker20 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.641, -15.570])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Cartografia imaginária na educação cartográfica.</h1>"))
+    .addTo(map);
+
+const marker21 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.622, -14.038])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Análise multitemporal do uso e cobertura do solo da região de influência do Parque Nacional da Chapada dos Veadeiros</h1>"))
+    .addTo(map);
+
+const marker22 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.847, -15.832])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Abordagem territorial da integração da política de recursos hídricos com a de meio ambiente na bacia hidrográfica do rio Paranoá.</h1>"))
+    .addTo(map);
+
+const marker23 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-63.581, -11.475])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Dinâmica do desmatamento em áreas protegidas no estado de Rondônia : estudo de caso do P.E. Guajará Mirim, Flona Bom Futuro e Resex Jaci-Paraná.</h1>"))
+    .addTo(map);
+
+const marker24 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.475, -15.953])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Dinâmica Espacial do Fluxo de Nascimentos e Óbitos Infantis no Distrito Federal em 2012.</h1>"))
+    .addTo(map);
+
+const marker25 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-41.300, -11.353])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Mapas participativos: quando os povos grafam seu mundo - O caso do mapeamento biorregional nas comunidades quilombolas do Kaonge, Dendê, Kalembá, Engenho da Ponte e Engenho da Praia.</h1>"))
+    .addTo(map);
+
+const marker26 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.620, -14.038])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Povos e comunidades tradicionais na Chapada dos Veadeiros, a saúde e os dados censitários</h1>"))
+    .addTo(map);
+
+const marker27 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.464, -15.959])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Monitoramento sazonal e recuperação pós-fogo da vegetação do Cerrado usando dados do sensor MODIS.</h1>"))
+    .addTo(map);
+
+const marker28 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.625, -14.038])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Uma breve análise do programa bolsa família na Chapada dos Veadeiros - GO.</h1>"))
+    .addTo(map);
+
+const marker29 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-52.726, -15.622])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Análise Geográfica do Índice de Desempenho do Sistema Único de Saúde Segundo Quadro Socioespacial e Econômico do Centro-Oeste.</h1>"))
+    .addTo(map);
+
+const marker30 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-51.954, -13.431])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Geoparques no Brasil: foco geográfico na solução dos desafios.</h1>"))
+    .addTo(map);
+
+const marker31 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.623, -14.033])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Uma Abordagem Geográfica sobre a Saúde na Chapada dos Veadeiros.</h1>"))
+    .addTo(map);
+
+const marker32 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-47.622, -14.043])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Distribuição Espaço Temporal: focos de calor detectados por sensores AVHRR e Modis na microregião da Chapada dos Veadeiros.</h1>"))
+    .addTo(map);
+
+const marker33 = new mapboxgl.Marker({
+    color: "greenyellow",
+}).setLngLat([-51.826, 3.843])
+    .setPopup(new mapboxgl.Popup().setHTML("<h1>Avaliação de políticas públicas de gestão do território a partir de um estudo sobre a fragmentação da paisagem nos municípios de Oiapoque e Calçoene, estado do Amapá.</h1>"))
+    .addTo(map);
+
